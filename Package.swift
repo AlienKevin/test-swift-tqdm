eec40fa70a7c5eeee9f56c14346dc0cb374260e6
Kevin Li <kevinli020508@gmail.com>
2023-09-27 15:29:10 -0400

    Reproduced Tqdm not found error

--- Package.swift
+++ Package.swift
@@ -4,11 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "test-swift-tqdm",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "test-swift-tqdm"),
-    ]
+  name: "test-swift-tqdm",
+  dependencies: [
+    .package(url: "https://github.com/ebraraktas/swift-tqdm.git", from: "0.1.2")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "test-swift-tqdm", dependencies: ["Tqdm"])
+  ]
 )
